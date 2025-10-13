6884fd9b2acd31a04b0cdb5076bd61c3567e85cf
Danira-Dy1224 <kushevdd@gmail.com>
2025-10-14 02:08:28 +0300

    adding some files from the previous lab work

--- circle.py
+++ circle.py
@@ -1,10 +1,17 @@
-import math
-
+import math;
 
 def area(r):
+    '''
+    Принимает значение r, где r (int) - радиус окружности
+    Возвращает площадь окружности (float)
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Принимает значение r, где r (int) - радиус окружности
+    Возвращает длину окружности (float)
+    '''
     return 2 * math.pi * r
 
